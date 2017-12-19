1604de755a5106a1efa25f166b3aabf5efbab137
Auston Stewart <auston@shedbuilt.net>
2017-12-18 16:32:39 -1000

    Removing bootstrap dir from build script.

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 patch -Np1 -i ${SHED_PATCHDIR}/kbd-2.0.4-backspace-1.patch
 sed -i 's/\(RESIZECONS_PROGS=\)yes/\1no/g' configure
 sed -i 's/resizecons.8 //' docs/man/man8/Makefile.in
-PKG_CONFIG_PATH=/tools/lib/pkgconfig ./configure --prefix=/usr --disable-vlock
+PKG_CONFIG_PATH=/usr/lib/pkgconfig ./configure --prefix=/usr --disable-vlock
 make -j $SHED_NUMJOBS
 make DESTDIR=${SHED_FAKEROOT} install
 mkdir -pv ${SHED_FAKEROOT}/usr/share/doc/kbd-2.0.4
