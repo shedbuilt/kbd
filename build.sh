92b1a41f83ea5d892f7451842c950eac04bf8c57
Auston Stewart <auston@shedbuilt.net>
2018-01-09 20:14:14 -0500

    Fixing bootstrap

--- build.sh
+++ build.sh
@@ -2,8 +2,14 @@
 patch -Np1 -i ${SHED_PATCHDIR}/kbd-2.0.4-backspace-1.patch
 sed -i 's/\(RESIZECONS_PROGS=\)yes/\1no/g' configure
 sed -i 's/resizecons.8 //' docs/man/man8/Makefile.in
-PKG_CONFIG_PATH=/usr/lib/pkgconfig ./configure --prefix=/usr --disable-vlock
-make -j $SHED_NUMJOBS
-make DESTDIR=${SHED_FAKEROOT} install
+if [ "$SHED_BUILDMODE" == 'bootstrap' ]; then
+    KBD_PKGCONFIG_PATH="/tools/lib/pkgconfig"
+else
+    KBD_PKGCONFIG_PATH="/usr/lib/pkgconfig"
+fi
+PKG_CONFIG_PATH="$KBD_PKGCONFIG_PATH" ./configure --prefix=/usr \
+                                                  --disable-vlock || exit 1
+make -j $SHED_NUMJOBS || exit 1
+make DESTDIR="$SHED_FAKEROOT" install || exit 1
 mkdir -pv ${SHED_FAKEROOT}/usr/share/doc/kbd-2.0.4
 cp -R -v docs/doc/* ${SHED_FAKEROOT}/usr/share/doc/kbd-2.0.4
